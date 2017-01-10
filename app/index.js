e3bbbaf25d846eb96c93d0a145437c9bea74726b
Jarno Nijboer <jnijboer@sintrum.nl>
2017-01-10 19:47:18 +0100

    WIP

--- app/index.js
+++ app/index.js
@@ -129,6 +129,9 @@ app.on('ready', () => installDevExtensions(isDev).then(() => {
       }
     }
 
+    const transparencyDisabled = true;
+
+
     const browserDefaults = {
       width,
       height,
@@ -139,7 +142,7 @@ app.on('ready', () => installDevExtensions(isDev).then(() => {
       backgroundColor: toElectronBackgroundColor(cfg.backgroundColor || '#000'),
       // we want to go frameless on windows and linux
       frame: process.platform === 'darwin',
-      transparent: true,
+      transparent: false,
       icon: resolve(__dirname, 'static/icon.png'),
       // we only want to show when the prompt is ready for user input
       // HYPERTERM_DEBUG for backwards compatibility with hyperterm
